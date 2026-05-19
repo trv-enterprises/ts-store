1f353a621038e14037274e0403d7e2f028f63087
Tom Viviano <tom.r.viviano@gmail.com>
2026-05-19 13:15:10 -0500

    Bump version to v0.6.5

--- cmd/tsstore/main.go
+++ cmd/tsstore/main.go
@@ -77,7 +77,7 @@ func main() {
 	case "help", "-h", "--help":
 		printUsage()
 	case "version", "-v", "--version":
-		fmt.Println("tsstore v0.6.4")
+		fmt.Println("tsstore v0.6.5")
 	default:
 		fmt.Printf("Unknown command: %s\n", command)
 		printUsage()
