6e956385f63f938ca398319b25ebc851a58e1725
Tom Viviano <tom.r.viviano@gmail.com>
2026-08-15 16:21:23 -0500

    Bump version to v0.20.2

--- internal/version/version.go
+++ internal/version/version.go
@@ -8,4 +8,4 @@ package version
 
 // Version is rewritten by `make version-bump` (see Makefile) during a
 // release; the sed there matches this exact declaration.
-var Version = "v0.20.1"
+var Version = "v0.20.2"
